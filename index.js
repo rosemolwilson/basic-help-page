16796fcc4f9f22d2cfb8000960b54b50abfdb209
Rosemol Wilson <rosemol.wilson@sap.com>
2023-09-23 20:30:51 +0530

    Moving header to a custom component

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-/**
-Challenge: 
-
-Part 2: 
-- Add a `header` element with a nested `nav` element. Inside the `nav`,
-  include a `img` element with the image of the React logo inside
-  (src="./react-logo.png") and make sure to set the width to something
-  more manageable so it doesn't take up the whole screen
-- Add an `h1` with some text describing the page. (E.g. "Reasons
-  I'm excited to learn React"). Place it above the ordered list.
-- Add a `footer` after the list that says: 
-    "© 20xx <last name here> development. All rights reserved."
- */
+function MyHeader() {
+    return (
+        <header>
+            <nav>
+                <img src="./images/react-logo.png" width="80px" />
+            </nav>
+        </header>
+    )
+}
 
 function Page() {
     return (
         <div>
-            <header>
-                <nav>
-                    <img src="./images/react-logo.png" width="80px" />
-                </nav>
-            </header>
+            <MyHeader />
             <h1>Reasons I'm excited to learn React!</h1>
             <ol>
                 <li>It's a popular library, so I'll be
