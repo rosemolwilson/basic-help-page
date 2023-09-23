8be9ad64921be1dc75140a43fb2294198a629893
Rosemol Wilson <rosemol.wilson@sap.com>
2023-09-23 20:08:12 +0530

    Solving a challenge

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-function MyPage() {
+/**
+Challenge: 
+
+Part 2: 
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+ */
+
+function Page() {
     return (
         <div>
-            <img src="./images/react-logo.png" width="80px" />
+            <header>
+                <nav>
+                    <img src="./images/react-logo.png" width="80px" />
+                </nav>
+            </header>
+            <h1>Reasons I'm excited to learn React!</h1>
             <ol>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100l starts on GitHub</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
+                <li>It's a popular library, so I'll be
+                    able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                    if I know React</li>
             </ol>
+            <small>© 2023 Wilson development. All rights reserved.</small>
         </div>
+
     )
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(<MyPage />)
+ReactDOM.render(<Page />, document.getElementById("root"))
