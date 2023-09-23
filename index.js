d4d9be9fe6a756b285b7ca26d0a187032513241a
Rosemol Wilson <rosemol.wilson@sap.com>
2023-09-23 19:59:24 +0530

    Using a custom component

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const image = <img src="./images/react-logo.png" width="80px"></img>
+function MyPage() {
+    return (
+        <div>
+            <img src="./images/react-logo.png" width="80px" />
+            <ol>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100l starts on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ol>
+        </div>
+    )
+}
 
-const pageContent = (
-    <div>
-        {image}
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100l starts on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
-)
-ReactDOM.render(pageContent, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")).render(<MyPage />)
